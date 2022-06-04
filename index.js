7eee666c5f5d757c74d699302e5b39cc99c64ef6
Samuel DW <s.durrantwalker@yordasgroup.com>
2022-06-04 21:49:02 +0100

    Adding comments and just checking some stuff works before using branches

--- index.js
+++ index.js
@@ -6,17 +6,27 @@ const mongoose = require('mongoose')
 const morgan = require('morgan')
 require('dotenv').config()
 
+/**
+ * Creating the application
+ */
 const app = express()
 
-
+/**
+ * For security purposes, the following will be enabled and configured
+ */
 app.disable('x-powered-by')
 app.use(helmet())
 app.use(cors())
+
 app.use(morgan("dev"))
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 
+/**
+ * Routing for the API
+ * Main routes will be Products, Orders, Order Items Users and Payments
+ */
 const productsRouter = require('./src/routes/products')
 
 app.use('/products', productsRouter)
--- src/controllers/products.js
+++ src/controllers/products.js
@@ -1,7 +1,12 @@
 exports.productList = (req ,res) => {
+    console.log(req.query)
     res.status(200).send({message: 'John'})
 }
 
 exports.createProduct = (req, res) => {
     res.status(200).send({message: 'What ho!'})
+}
+
+exports.deleteProduct = (req, res) => {
+    res.status(200).send()
 }
