d6aefee174203e9fd06e61b8a81a2282d693f8bc
Samuel DW <s.durrantwalker@yordasgroup.com>
2022-06-05 10:06:42 +0100

    Added example schema

---
+++ src/models/product.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+
+const ProductSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    }
+})
+
+
+const Product = mongoose.model('Product', ProductSchema)
+
+module.exports = Product
